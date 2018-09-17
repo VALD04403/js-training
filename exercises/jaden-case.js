b0053a17def9170e1ae2ae8d43be90bfb7e136dd
Valentin Berruer <valb37@icloud.com>
2018-09-17 23:31:13 +0200

    TDD4

--- exercises/jaden-case.js
+++ exercises/jaden-case.js
@@ -9,11 +9,16 @@
  * Example : "How are you ?" -> "How Are You ?"
  *
  */
+function jadenCase(string){
+	return string.split(" ").map(function(word){
+		return word;charAt(0).toUpperCase() + word.slice(1);
 
-
+	}) .join(" ");
+}
 
 //* Begin of tests
 const assert = require('assert')
 
-assert.fail('You must write your own tests')
+assert.deepStrictEqual(jadenCase("How Are You"), 'How Are You')
+assert.deepStrictEqual(jadenCase("Hi Guys"), 'Hi Guys')
 // End of tests */
